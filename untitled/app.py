eca6f8a8f65fce2befc7c6cb021576bb5a3efb65
lasloh <laslo.hauschild@web.de>
2023-04-12 21:20:03 +0200

    Added server as module

--- untitled/app.py
+++ untitled/app.py
@@ -1,15 +1,14 @@
-from flask import Flask, abort, redirect, url_for, render_template, request
-import json
+from flask import Flask, abort, request
 
 app = Flask(__name__)
 
 
 class Ticket:
-    def __init__(self, ticketid, tickettype, isvalid, isdevaluated):
-        self.ticketid = ticketid
-        self.tickettype = tickettype
-        self.isvalid = isvalid
-        self.isdevaluated = isdevaluated
+    def __init__(self, ticket_id, ticket_type, is_valid, is_devaluated):
+        self.ticket_id = ticket_id
+        self.ticket_type = ticket_type
+        self.is_valid = is_valid
+        self.is_devaluated = is_devaluated
 
 
 def toJSON(ticketid, tickettype, isvalid, isdevaluated):
@@ -27,11 +26,11 @@ def index():
     if request.args.get("auth") != "authkey":
         return abort(401)
     for x in mylist:
-        if x.ticketid == request.args.get("d"):
-            if x.isdevaluated == False:
-                x.isdevaluated = True
-                return toJSON(x.ticketid, x.tickettype, x.isvalid, False)
-            return toJSON(x.ticketid, x.tickettype, x.isvalid, x.isdevaluated)
+        if x.ticket_id == request.args.get("d"):
+            if x.is_devaluated == False:
+                x.is_devaluated = True
+                return toJSON(x.ticket_id, x.ticket_type, x.is_valid, False)
+            return toJSON(x.ticket_id, x.ticket_type, x.is_valid, x.is_devaluated)
     return toJSON(request.args.get("d"), "unknown", False, False)
 
 @app.route("/a")
