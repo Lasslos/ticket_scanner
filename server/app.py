9f89e5448819d585cff9e0500188e5d730a2d76d
Caspar <Caspar.thielmann@outlook.com>
2023-04-13 14:22:43 +0200

    Made changes to the server: The server is now able to read in a json file placed in Server/Resources named tickets.json. After devaluating the tickets, the changes will be saved to the json-file.

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, request, send_file
 
 app = Flask(__name__)
@@ -12,37 +13,40 @@ class Ticket:
 
 
 def toJSON(ticketid, tickettype, isvalid, isdevaluated):
-    return "{\"ticketID\": \"" + ticketid + "\",\"ticketType\" : \"" + tickettype + "\",\n\"isValid\" : " + str(isvalid).lower() + ",\n\"isDevaluated\" : " + str(isdevaluated).lower() + "\n}"
-
-
-d1 = Ticket('1', "regular", True, False)
-d2 = Ticket('2', "reduced", True, False)
-d3 = Ticket('3', "volunteer", True, False)
-d4 = Ticket('4', "regular", True, False)
-mylist = [d1, d2, d3, d4]
-
-thisdict = {
-  "1": d1,
-  "2": d2,
-  "3": d3,
-  "4": d4
-}
+    return "{\"ticketID\": \"" + ticketid + "\",\"ticketType\" : \"" + tickettype + "\",\n\"isValid\" : " + str(
+        isvalid).lower() + ",\n\"isDevaluated\" : " + str(isdevaluated).lower() + "\n}"
 
 
 @app.route("/")
 def index():
     ticket_id_to_validate = request.args.get("d")
+    read_from_json()
     if thisdict.__contains__(ticket_id_to_validate) == False:
         return toJSON(request.args.get("d"), "unknown", False, False)
-    if thisdict[ticket_id_to_validate].is_devaluated == False:
-        thisdict[ticket_id_to_validate].is_devaluated = True
-        return toJSON(thisdict[ticket_id_to_validate].ticket_id, thisdict[ticket_id_to_validate].ticket_type, thisdict[ticket_id_to_validate].is_valid, False)
-    return toJSON(thisdict[ticket_id_to_validate].ticket_id, thisdict[ticket_id_to_validate].ticket_type, thisdict[ticket_id_to_validate].is_valid, thisdict[ticket_id_to_validate].is_devaluated)
+    if thisdict[ticket_id_to_validate]["is_devaluated"] == False:
+        thisdict[ticket_id_to_validate]["is_devaluated"] = True
+        write_to_json()
+        return toJSON(thisdict[ticket_id_to_validate]["ticket_id"], thisdict[ticket_id_to_validate]["ticket_type"],
+                      thisdict[ticket_id_to_validate]["is_valid"], False)
+    return toJSON(thisdict[ticket_id_to_validate]["ticket_id"], thisdict[ticket_id_to_validate]["ticket_type"],
+                  thisdict[ticket_id_to_validate]["is_valid"], thisdict[ticket_id_to_validate]["is_devaluated"])
 
 
 @app.route("/app")
-def a():
+def deliver_app():
     return send_file("C:/Flutter/Projects/ticket_scanner/build/app/outputs/flutter-apk/app-release.apk")
+
+
+def write_to_json():
+    with open("Resources/tickets.json", "w") as fo:
+        fo.write(json.dumps(thisdict, default=lambda o: o.__dict__, indent=4))
+
+
+def read_from_json():
+    with open('Resources/tickets.json') as json_file:
+        global thisdict
+        thisdict = json.load(json_file)
+    #print(type(thisdict["1"]))
 
 
 if __name__ == "__main__":
