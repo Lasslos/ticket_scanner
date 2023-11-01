b0a5e63434c22c7cdf5f2b462437ff43f2509be5
lasloh <laslo.hauschild@web.de>
2023-11-01 19:34:40 +0100

    Added db models and connection structure

---
+++ server/models.py
@@ -0,0 +1,28 @@
+import enum
+
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Enum
+
+from database import dbBase
+
+
+class TicketType(str, enum.Enum):
+    student = "student"
+    volunteer = "volunteer"
+
+
+class Ticket(dbBase):
+    __tablename__ = "tickets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    type = Column(Enum(TicketType))
+    created = Column(DateTime, server_default="func.now()")
+    notes = Column(String, default="")
+    entered = Column(DateTime, nullable=True)
+    is_present = Column(Boolean, default=False)
+
+
+
+class User(dbBase):
+    username = Column(String, primary_key=True, index=True)
+    hashed_password = Column(String)
