547671a6a9fc8fa21e9d51aee578d330baa3ec55
lasloh <laslo.hauschild@web.de>
2023-11-07 19:43:02 +0100

    Removed functional crimes from code ._.

--- flutter/lib/screens/actions/create_ticket.dart
+++ flutter/lib/screens/actions/create_ticket.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ticket_scanner/core/models/ticket.dart';
 import 'package:ticket_scanner/core/provider/ticket_provider.dart';
+import 'package:ticket_scanner/screens/shared/form_fields.dart';
 import 'package:ticket_scanner/screens/shared/ticket_widget.dart';
 import 'package:ticket_scanner/util/logger.dart';
 
@@ -56,14 +57,9 @@ class _CreateTicketState extends ConsumerState<CreateTicket> {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          TextFormField(
+          NameField(
             focusNode: focusNodes[0],
-            autocorrect: false,
-            keyboardType: TextInputType.name,
-            textInputAction: TextInputAction.next,
-            onEditingComplete: () {
-              FocusScope.of(context).requestFocus(focusNodes[2]);
-            },
+            nextFocusNode: focusNodes[1],
             validator: (value) {
               if (value == null || value.isEmpty) {
                 return 'Bitte gib einen Namen ein.';
@@ -73,82 +69,50 @@ class _CreateTicketState extends ConsumerState<CreateTicket> {
             onSaved: (value) {
               _ticketCreate = _ticketCreate.copyWith(name: value!);
             },
-            decoration: const InputDecoration(
-              labelText: 'Name',
-            ),
           ),
           const SizedBox(height: 16),
           SizedBox(
             width: double.infinity,
-            child: SegmentedButton(
-              segments: TicketType.values.map(
-                (type) => ButtonSegment(
-                  value: type,
-                  icon: Icon(type.icon),
-                  label: Text(type.name),
-                ),
-              ).toList(),
-              selected: { _ticketCreate.type },
-              onSelectionChanged: (selection) {
-                setState(() {
-                  _ticketCreate = _ticketCreate.copyWith(type: selection.first);
-                });
-              },
+            child: TicketTypeSelection(
+              selected: _ticketCreate.type,
+              onSelectionChanged: (selected) => setState(() {
+                _ticketCreate = _ticketCreate.copyWith(type: selected);
+              }),
             ),
           ),
           const SizedBox(height: 8),
-          TextFormField(
+          NotesField(
             focusNode: focusNodes[1],
-            autocorrect: true,
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-            textInputAction: TextInputAction.newline,
-            enableSuggestions: true,
-            onEditingComplete: () {
-              FocusScope.of(context).requestFocus(focusNodes[3]);
-            },
-            decoration: const InputDecoration(
-              labelText: 'Notizen',
-            ),
+            nextFocusNode: focusNodes[2],
             onSaved: (value) {
               _ticketCreate = _ticketCreate.copyWith(notes: value);
             },
           ),
           const SizedBox(height: 48),
-
-          Row(
-            children: [
-              if (_error != null && !isLoading) SizedBox(
-                width: MediaQuery.of(context).size.width * 0.6,
-                child: Text(
-                  _error!,
-                  style: Theme.of(context).textTheme.labelLarge?.copyWith(color: Theme.of(context).colorScheme.error),
-                ),
-              ),
-              if (isLoading) const LinearProgressIndicator(),
-              const Spacer(),
-              ElevatedButton(
-                focusNode: focusNodes[2],
-                onPressed: () async {
-                  if (_formKey.currentState!.validate() && widget.id != null) {
-                    _formKey.currentState!.save();
-                    try {
-                      await ref.read(ticketsProvider(widget.id!).notifier).createTicket(_ticketCreate);
-                    } catch (e, s) {
-                      getLogger().e('Failed to create ticket', error: e, stackTrace: s);
-                      setState(() {
-                        _error = e.toString();
-                      });
-                      return;
-                    }
+          ErrorSubmitRow(
+            error: _error,
+            isLoading: isLoading,
+            submitButton: ElevatedButton(
+              focusNode: focusNodes[2],
+              onPressed: () async {
+                if (_formKey.currentState!.validate() && widget.id != null) {
+                  _formKey.currentState!.save();
+                  try {
+                    await ref.read(ticketsProvider(widget.id!).notifier).createTicket(_ticketCreate);
+                  } catch (e, s) {
+                    getLogger().e('Failed to create ticket', error: e, stackTrace: s);
                     setState(() {
-                      _error = null;
+                      _error = e.toString();
                     });
+                    return;
                   }
-                },
-                child: const Text('Verkaufen'),
-              ),
-            ],
+                  setState(() {
+                    _error = null;
+                  });
+                }
+              },
+              child: const Text('Verkaufen'),
+            ),
           ),
           if (ticket != null) const Divider(height: 32),
           if (ticket != null) Card(
---
+++ flutter/lib/screens/shared/form_fields.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_scanner/core/models/ticket.dart';
+
+// Builds a TextFormField suitable for name imput.
+// Takes a focusNode of the textField, a focusNode of the next form field,
+// and a validate and an onSaved function.
+class NameField extends StatelessWidget {
+  final FocusNode focusNode;
+  final FocusNode nextFocusNode;
+  final String? Function (String?)? validator;
+  final Function (String?)? onSaved;
+
+  const NameField({
+    required this.focusNode,
+    required this.nextFocusNode,
+    required this.validator,
+    required this.onSaved,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => TextFormField(
+    focusNode: focusNode,
+    autocorrect: false,
+    keyboardType: TextInputType.name,
+    textInputAction: TextInputAction.next,
+    onEditingComplete: () {
+      FocusScope.of(context).requestFocus(nextFocusNode);
+    },
+    validator: validator,
+    onSaved: onSaved,
+    decoration: const InputDecoration(
+      labelText: 'Name',
+    ),
+  );
+}
+
+
+// Builds a TextFormField suitable for ticket type selection.
+// Takes a selected ticket type, a function to call when the selection changes,
+// and a bool to allow the selection of no ticket.
+class TicketTypeSelection extends StatelessWidget {
+  final TicketType selected;
+  final Function (TicketType) onSelectionChanged;
+  final bool allowNone;
+
+  const TicketTypeSelection({
+    required this.selected,
+    required this.onSelectionChanged,
+    this.allowNone = false,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final List<ButtonSegment<TicketType?>> segments = [
+      if (allowNone) const ButtonSegment(
+        value: null,
+        icon: Icon(Icons.remove),
+        label: Text('Kein Ticket'),
+      ),
+      ...TicketType.values.map(
+            (type) => ButtonSegment(
+          value: type,
+          icon: Icon(type.icon),
+          label: Text(type.name),
+        ),
+      ),
+    ];
+    return SegmentedButton(
+      segments: segments,
+      selected: { selected },
+      onSelectionChanged: (selection) {
+        onSelectionChanged(selection.first!);
+      },
+    );
+  }
+}
+
+// Builds a TextFormField suitable for notes imput.
+// Takes a focusNode of the textField, a focusNode of the next form field,
+// and an onSaved function.
+class NotesField extends StatelessWidget {
+  final FocusNode focusNode;
+  final FocusNode nextFocusNode;
+  final Function (String?)? onSaved;
+
+  const NotesField({
+    required this.focusNode,
+    required this.nextFocusNode,
+    required this.onSaved,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => TextFormField(
+      focusNode: focusNode,
+      autocorrect: true,
+      keyboardType: TextInputType.multiline,
+      maxLines: null,
+      textInputAction: TextInputAction.newline,
+      enableSuggestions: true,
+      onEditingComplete: () {
+        FocusScope.of(context).requestFocus(nextFocusNode);
+      },
+      decoration: const InputDecoration(
+        labelText: 'Notizen',
+      ),
+      onSaved: onSaved,
+  );
+}
+
+
+// Builds a TextFormField suitable for ticket price input.
+// Takes a focusNode of the textField, a focusNode of the next form field,
+// and a validate and an onSaved function.
+class ErrorSubmitRow extends StatelessWidget {
+  final String? error;
+  final bool isLoading;
+  final Widget submitButton;
+
+  const ErrorSubmitRow({
+    required this.submitButton,
+    required this.error,
+    this.isLoading = false,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => Row(
+    children: [
+      if (error != null && !isLoading) SizedBox(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: Text(
+          error!,
+          style: Theme.of(context).textTheme.labelLarge?.copyWith(color: Theme.of(context).colorScheme.error),
+        ),
+      ),
+      if (isLoading) const LinearProgressIndicator(),
+      const Spacer(),
+      submitButton,
+    ],
+  );
+}
